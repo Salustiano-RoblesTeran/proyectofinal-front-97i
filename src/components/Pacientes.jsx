477bbb77c9240f7d067e374e3fe6432a197c21de
“Magali <maguinavarro07@gmail.com>
2024-08-31 21:27:45 -0300

    Agregado de paginacion y modal

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
-
-const Paciente = ({ paciente, onAceptar, onRechazar }) => {
+const Paciente = ({ paciente, onAceptar, onRechazar, onVerHistoriaClinica }) => {
     return (
-        <tr>
+        <tr >
             <td>{paciente.nombre}</td>
             <td>{paciente.apellido}</td>
             <td>{paciente.email}</td>
@@ -15,6 +14,13 @@ const Paciente = ({ paciente, onAceptar, onRechazar }) => {
                 <td>
                     <button onClick={() => onRechazar(paciente.id)} className="btn btn-danger btn-sm">Rechazar</button>
                     <button onClick={() => onAceptar(paciente.id)} className="btn btn-success btn-sm">Aceptar</button>
+                </td>
+            )}
+            {!onAceptar && !onRechazar && (
+                <td>
+                    <button onClick={() => onVerHistoriaClinica(paciente)} className="btn btn-success btn-sm">
+                        Ver Historia Clínica
+                    </button>
                 </td>
             )}
         </tr>
