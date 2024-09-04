9707bd0aa334efb3a5beee83c88c20b78228976a
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-09-04 01:00:55 -0300

    Agregue la seccion pasos en el home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
-import Footer from "../Components/Common/Footer"
+
 import NavBar from "../Components/Common/NavBar"
 import CardSlider from "../Components/Home/CardSlider"
+import Pasos from "../Components/Home/Pasos"
 
 const Home = () => {
   return (
@@ -9,8 +10,9 @@ const Home = () => {
     <div className="App">
       <h1 className="text-center my-4">Nuestros Médicos</h1>
       <CardSlider/>
+      <Pasos/>
     </div>
-        <Footer/>
+        
     </>
 
   )
