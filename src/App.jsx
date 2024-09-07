e34e7cc281cd397c2c5681254c88a9417cc628c5
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-09-07 12:44:26 -0300

    resolvi conflictos

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,17 @@
-import React, { useState } from 'react';
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './App.css'
+import Home from './Pages/Home'
 
-import TablaPacientes from '../src/components/TablaPacientes';
+function App() {
 
-const App = () => {
-    const [pacientes, setPacientes] = useState([]);
-    const [pacientesAceptados, setPacientesAceptados] = useState([]);
-    const [pacientesRechazados, setPacientesRechazados] = useState([]);
-
-    const agregarPaciente = (nuevoPaciente) => {
-        setPacientes([...pacientes, nuevoPaciente]);
-    };
-
-    const aceptarPaciente = (id) => {
-        const pacienteAceptado = pacientes.find(paciente => paciente.id === id);
-        setPacientes(pacientes.filter(paciente => paciente.id !== id));
-        setPacientesAceptados([...pacientesAceptados, pacienteAceptado]);
-    };
-
-    const rechazarPaciente = (id) => {
-        const pacienteRechazado = pacientes.find(paciente => paciente.id === id);
-        setPacientes(pacientes.filter(paciente => paciente.id !== id));
-        setPacientesRechazados([...pacientesRechazados, pacienteRechazado]);
-    };
-
-    return (
-        <div className="container">
-            <h1 className="text-center my-4">Gestión de Pacientes</h1>
-            <h2>Pacientes Pendientes</h2>
-            <TablaPacientes pacientes={pacientes} onAceptar={aceptarPaciente} onRechazar={rechazarPaciente} />
-            <h2>Pacientes Aceptados</h2>
-            <TablaPacientes pacientes={pacientesAceptados} />
-            <h2>Pacientes Rechazados</h2>
-            <TablaPacientes pacientes={pacientesRechazados} />
-        </div>
-    );
-};
-
-export default App;
-
-
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
 
