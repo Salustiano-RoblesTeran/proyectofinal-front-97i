86fe0f4dc526aa06036634ee750a4c06794d52a9
Gonzalo <gonchimartinez9@gmail.com>
2024-09-09 18:00:56 -0300

    solucionando problema

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-=======
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
->>>>>>> PanelUsuario
 
 import App from "./App";
 import "./App.css";
