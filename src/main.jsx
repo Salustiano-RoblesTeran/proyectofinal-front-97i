0c38b4f123027110c593318bcda056e2b51fb611
Gonzalo <gonchimartinez9@gmail.com>
2024-09-09 18:17:59 -0300

    prueba del codigo

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import App from "./App";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+import App from "./App";
+import "./App.css";
