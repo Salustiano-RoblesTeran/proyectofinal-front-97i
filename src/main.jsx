19f1b638f1c96db444e0d564cca1326de791c55a
Gonzalo <gonchimartinez9@gmail.com>
2024-09-09 18:22:24 -0300

    viendo main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -12,7 +10,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-
-
 import App from "./App";
 import "./App.css";
