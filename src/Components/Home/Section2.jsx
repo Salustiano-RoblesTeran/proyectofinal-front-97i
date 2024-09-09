54b3fc965dfa1c0b8a8ad430bde00e3f2171f188
gero rios <antenuccifc87@gmail.com>
2024-09-09 11:13:06 -0300

    Finalizo la integridad de mis ramas con la dev y arreglo problemas

--- src/Components/Home/Section2.jsx
+++ src/Components/Home/Section2.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, Accordion } from 'react-bootstrap';
 
 const Section2 = () => {
   const [medicos, setMedicos] = useState([]);
@@ -70,8 +70,10 @@ const Section2 = () => {
   return (
     <Container>
       <Row>
-        <Col lg={6}>
+        <Col lg={6} id=''>
           <h2>Solicitar Turnos</h2>
+          <hr className="w-25"/>
+          <p>Rellena el formulario y solicita el turno con el medicos que necesites!</p>
           <Form onSubmit={handleSubmit}>
             <Row className="mb-3">
               <Col md={6}>
@@ -161,6 +163,44 @@ const Section2 = () => {
               Solicitar Turno
             </Button>
           </Form>
+        </Col>
+
+        <Col lg={6} className="mt-4 mt-lg-0">
+          <h2>Preguntas Frecuentes</h2>
+          <hr className="w-25"/>
+          <p>Algunas de las preguntas que nuestros pacientes suelen tener...</p>
+          <Accordion className='mt-4'>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>¿Que es un estudio de alta complejidad?</Accordion.Header>
+              <Accordion.Body>
+              Son estudios de Resonancia Magnética, Tomografía Computada ó Mamografía Digital que profundizan en la exploración diagnóstica para una evaluación mas completa de los tejidos..
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>¿Es necesaria internacion?</Accordion.Header>
+              <Accordion.Body>
+              No. Los estudios se realizan de manera ambulatoria, es decir que el paciente se va el mismo día que se realiza la práctica. Las prácticas pueden durar entre 45 minutos y 1 hora dependiendo el estudio.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>¿Que preparacion debo realizar?</Accordion.Header>
+              <Accordion.Body>
+                Depende el estudio a realizarse.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>¿Cuando recibire mis resultados?</Accordion.Header>
+              <Accordion.Body>
+              Los resultados suelen estar disponibles entre 24 y 48 horas después del examen. Su médico recibirá los resultados y los discutirá con usted en su cita de seguimiento.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Mi medico realizo un pedido ¿todavia sirve?</Accordion.Header>
+              <Accordion.Body>
+              Las órdenes ó prescripciones médicas tienen una validez de 30 días. Debiendo contar con la siguiente información: Datos del paciente y obra social, práctica a realizar, diagnóstico, fecha, firma y sello del médico especialista que lo deriva.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </Col>
       </Row>
     </Container>
--- src/Pages/HomeScreen.jsx
+++ src/Pages/HomeScreen.jsx
@@ -2,14 +2,20 @@ import React from "react";
 
 import CardSlider from "../Components/Home/CardSlider"
 import Pasos from "../Components/Home/Pasos"
-
+import HeroSection from "../Components/Home/HeroSection";
+import SectionInfo from "../Components/Home/SectionInfo";
+import Section2 from "../Components/Home/Section2"
 const HomeScreen = () => {
   return (
     <>
     <div className="App">
+      <HeroSection/>
+      <SectionInfo />
+      <Pasos/>
+      <Section2 />
       <h1 className="text-center my-4">Nuestros Médicos</h1>
       <CardSlider/>
-      <Pasos/>
+
     </div>
         
     </>
