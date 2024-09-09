f9eac6f6335d7e9574eb448b61e743e26c5d9b5e
Gonzalo <gonchimartinez9@gmail.com>
2024-09-09 17:01:21 -0300

    resolviendo problema main

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-const main = () => {
-  return <div>main</div>;
-};
-
-export default main;
+import App from "./App";
+import "./App.css";
