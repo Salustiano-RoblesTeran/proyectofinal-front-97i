e12a904d4d9f5e4d7da2f76034c0a3269f1bb827
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-10-29 22:27:27 -0300

    Corrigiendo seccion Pacientes

--- src/Components/PanelMedicos/Pacientes.jsx
+++ src/Components/PanelMedicos/Pacientes.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 const Paciente = ({ paciente, onAceptar, onRechazar, onVerHistoriaClinica }) => {
     return (
         <tr>
-        <td>{paciente.user.name}</td>
-        <td>{paciente.user.last_name}</td>
-        <td>{paciente.user.email}</td>
-        <td>{paciente.user.phone_number}</td>
-        <td>{paciente.tipoEstudio.name}</td>
-        <td>{new Date(paciente.fecha).toLocaleString()}</td>
-        {onAceptar && onRechazar && (
-          <td>
-            <button className="btn btn-success m-1" onClick={() => onAceptar(paciente._id)}>Aceptar</button>
-            <button className="btn btn-danger" onClick={() => onRechazar(paciente._id)}>Rechazar</button>
-          </td>
-        )}
-      </tr>
+            <td>{paciente.user?.name || 'N/A'}</td>
+            <td>{paciente.user?.last_name || 'N/A'}</td>
+            <td>{paciente.user?.email || 'N/A'}</td>
+            <td>{paciente.user?.phone_number || 'N/A'}</td>
+            <td>{paciente.tipoEstudio?.name || 'N/A'}</td>
+            <td>{new Date(paciente.fecha).toLocaleString()}</td>
+            {onAceptar && onRechazar && (
+                <td>
+                    <button className="btn btn-success m-1" onClick={() => onAceptar(paciente._id)}>Aceptar</button>
+                    <button className="btn btn-danger" onClick={() => onRechazar(paciente._id)}>Rechazar</button>
+                </td>
+            )}
+        </tr>
     );
 };
 
